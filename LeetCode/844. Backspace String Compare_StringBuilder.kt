de4017fb8ede88e86996da2d666797f770c78410
B.N <19542357+BNSC1@users.noreply.github.com>
2022-10-18 09:45:15 +0800

    - Update: Backspace String Compare in String Builder

--- LeetCode/844. Backspace String Compare_StringBuilder.kt
+++ LeetCode/844. Backspace String Compare_StringBuilder.kt
@@ -5,18 +5,16 @@ class Solution {
         var bs = 0
 
         for (i in s.length-1 downTo 0) {
-            if (bs <= 0 && s[i] != '#') {
-                sbS.append(s[i])
-            } else if (s[i] == '#') bs++ //if the char is a backspace, add backspace count by 1
-              else if (bs > 0) bs-- //if the char isn't a backspace but there is a backspace, subtract backspace count by 1
+            if (s[i] == '#') bs++ //if the char is a backspace, add backspace count by 1
+            else if (bs > 0) bs-- //if the char isn't a backspace but there is a backspace, subtract backspace count by 1
+            else sbS.append(s[i])
         }
         bs = 0 // reset backspace count in case there are more backspaces than actual chars from previous loop
 
         for (i in t.length-1 downTo 0) {
-            if (bs <= 0 && t[i] != '#') {
-                sbT.append(t[i])
-            } else if (t[i] == '#') bs++
-              else if (bs > 0) bs--
+            if (t[i] == '#') bs++
+            else if (bs > 0) bs--
+            else sbT.append(t[i])
         }
 
         return sbS.toString() == sbT.toString() //compare if both result strings are the same
