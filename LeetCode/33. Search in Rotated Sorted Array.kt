15424958de20879c9d92e3f536cd1a344314f7f2
B.N <19542357+BNSC1@users.noreply.github.com>
2022-11-10 10:07:01 +0800

    - Update: Search in Rotated Sorted Array

--- LeetCode/33. Search in Rotated Sorted Array.kt
+++ LeetCode/33. Search in Rotated Sorted Array.kt
@@ -40,33 +40,33 @@ class Solution {
 
             } else {
                 /* if start value and mid value are not within the same "ramp", means mid value and end value are within the same one
+                * |   /
+                * |  /
+                * |        /
+                * |       e
                 * |      /
-                * |     /
-                * |           /
-                * |          e
-                * |         /
-                * |        m
-                * |       /
+                * |     m
+                * |    /
                 * -----------*/
                 if (target < nums[m] || target > nums[e]) e = m - 1
                 /* when target is within the same ramp, or not within, make end pointer m - 1
-                * |      /         |      T
-                * |     /          |     /
-                * |           /    |           /
-                * |          e  or |          e
-                * |         /      |         /
-                * |        m       |        m
-                * |       T        |       /
-                * -----------      -----------*/
+                * |   /         |   T
+                * |  /          |  /
+                * |        /    |        /
+                * |       e  or |       e
+                * |      /      |      /
+                * |     m       |     m
+                * |    T        |    /
+                * -----------   -----------*/
                 else s = m + 1
                 /* if target is between mid and end, make start pointer m + 1
-                * |      /
-                * |     /
-                * |           /
-                * |          e
-                * |         T
-                * |        m
-                * |       /
+                * |   /
+                * |  /
+                * |        /
+                * |       e
+                * |      T
+                * |     m
+                * |    /
                 * -----------*/
             }
         }
