90b8a79d9511e85d54d374e157c3539daebd753e
B.N <19542357+BNSC1@users.noreply.github.com>
2023-02-26 09:33:36 +0800

    - Add: Generate Parentheses

---
+++ LeetCode/22. Generate Parentheses.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun generateParenthesis(n: Int): List<String> {
+        val res = mutableListOf<String>()
+        val str = ArrayDeque<Char>()
+
+        fun generate(open: Int, close: Int) {
+            if (open == 0 && close == 0) { //when this branch reaches to the end (no more parentheses remaining)
+                res.add(str.reversed().joinToString(separator = ""))  //add to the result
+                return //close this branch
+            }
+            if (open > 0) { 
+                str.push('(')
+                generate(open-1, close) //start a branch with an open parentheses
+                str.pop() //restore stack
+            }
+            if (close > open) { //if there are more closed parentheses remaining
+                str.push(')')
+                generate(open, close-1) //start a branch with a closed parentheses
+                str.pop() //restore stack
+            }
+        }
+
+        generate(n, n)
+        return res
+    }
+}
