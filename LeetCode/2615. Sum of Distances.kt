8bc0e5006f707e3f3a5693ce6e986740160d0412
B.N <19542357+BNSC1@users.noreply.github.com>
2023-04-10 19:43:58 +0800

    - Add: Sum of Distances

---
+++ LeetCode/2615. Sum of Distances.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun distance(nums: IntArray): LongArray {
+        val map = HashMap<Int, MutableList<Int>>() //val: indexes
+        nums.forEachIndexed { i, n ->
+            map[n] = map[n]?.apply {add(i)} ?: mutableListOf(i)
+        }
+
+        val res = LongArray(nums.size)
+        map.forEach { (n, indexes) ->
+            var sum = 0L
+            for (i in 0 until indexes.size) {
+                sum += indexes[i] - indexes[0]
+            }
+            res[indexes[0]] = sum
+            for (i in 1 until indexes.size) {
+                sum += (indexes[i] - indexes[i-1]) * (i + i - indexes.size)
+                res[indexes[i]] = sum
+            }
+        }
+        return res
+    }
+}
