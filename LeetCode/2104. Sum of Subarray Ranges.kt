6a8c8ccba241545cb98e01f5b9c9604ff5576e78
B.N <19542357+BNSC1@users.noreply.github.com>
2023-06-09 14:21:38 +0800

    - Update: Sum of Subarray Ranges

--- LeetCode/2104. Sum of Subarray Ranges.kt
+++ LeetCode/2104. Sum of Subarray Ranges.kt
@@ -1,32 +1,31 @@
 class Solution {
-    fun subArrayRanges(nums: IntArray) = sumOf(nums, Target.LARGEST) - sumOf(nums, Target.SMALLEST)
-
-    private fun sumOf(nums: IntArray, target: Target): Long {
-        var res = 0L
-        val stack = LinkedList<Pair<Int, Int>>().apply { push(-1 to target.xVal) } //num, index
-        fun popAt(index: Int) = stack.pop().let { (popIndex, popNum) ->
-            res += popNum.toLong() * (popIndex - stack.peek().first) * (index - popIndex)
-        }
-        nums.forEachIndexed { index, num ->
-            while (target.extremeOf(num, stack.peek().second) == num) {
-                popAt(index)
+    fun subArrayRanges(nums: IntArray): Long {
+        fun sumOf(target: Target): Long {
+            var res = 0L
+            val st = LinkedList<Pair<Int,Int>>().apply { push(-1 to target.xVal) }
+            fun popAt(i: Int) = st.pop().let { (popI, popN) ->
+                res += popN.toLong() * (i - popI) * (popI - st.peek().first)
             }
-            stack.push(index to num)
+            nums.forEachIndexed { i, n ->
+                while(target.extremeOf(st.peek().second, n) == n) {
+                    popAt(i)
+                }
+                st.push(i to n)
+            }
+            while (st.size > 1) {
+                popAt(nums.size)
+            }
+            return res
         }
-        while (stack.size > 1) {
-            popAt(nums.size)
-        }
-        return res
+        return sumOf(Target.LARGEST) - sumOf(Target.SMALLEST)
     }
-
-    private enum class Target(val xVal: Int) {
+    enum class Target(val xVal: Int) {
         LARGEST(Int.MAX_VALUE) {
             override fun extremeOf(a: Int, b: Int) = maxOf(a, b)
         },
         SMALLEST(Int.MIN_VALUE) {
             override fun extremeOf(a: Int, b: Int) = minOf(a, b)
         };
-
         abstract fun extremeOf(a: Int, b: Int): Int
     }
 }
