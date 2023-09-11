cb9461b37db72ab18b0a612362643c6bc7891c73
B.N <19542357+BNSC1@users.noreply.github.com>
2023-09-11 08:32:09 +0800

    - Add solutions

---
+++ LeetCode/2850. Minimum Moves to Spread Stones Over Grid.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun minimumMoves(grid: Array<IntArray>): Int {
+        // Base Case
+        var empties = 0
+        repeat(3) { r -> repeat(3) { c ->
+                if (grid[r][c] == 0) empties++
+        } }
+        if (empties == 0) return 0
+        
+        var res = Int.MAX_VALUE
+        repeat(3) { emptyR -> repeat(3) { emptyC ->
+            if (grid[emptyR][emptyC] == 0) {
+                repeat(3) { spareR -> repeat(3) { spareC ->
+                    val distance = abs(spareR - emptyR) + abs(spareC - emptyC)
+                    if (grid[spareR][spareC]-- > 1) {
+                        grid[emptyR][emptyC]++
+                        res = min(res, distance + minimumMoves(grid)) //backtrack
+                        grid[spareR][spareC]++
+                        grid[emptyR][emptyC]--
+                    }
+                } }
+            }
+        } }
+        return res
+    }
+}
